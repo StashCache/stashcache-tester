07bd72e5ce3f1e224703b0a096bb41b5737122ce
Derek Weitzel <djw8605@gmail.com>
2015-08-20 14:15:44 -0500

    bumping version in setup.py

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ import glob
 
 setup(
     name = "stashcache_tester",
-    version = "0.0.1.2",
+    version = "0.0.1.3",
     author = "Derek Weitzel",
     author_email = "dweitzel@cse.unl.edu",
     description = ("A tester for the StashCache infrastructure"),
